63b01f8932cfaa9e4e3469167fd85bb076b8a980
zerofour4 <87287030+zerofour4@users.noreply.github.com>
2021-09-27 15:33:40 +0900

    init

---
+++ src/test/java/com/f/s5/DataSourceTest.java
@@ -0,0 +1,29 @@
+package com.f.s5;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class DataSourceTest extends MyJunitTest{
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Test
+	public void test()throws Exception {
+		assertNotNull(dataSource.getConnection());
+	}
+	
+	@Test
+	public void sqlSessionTest() {
+		assertNotNull(sqlSession);
+	}
+
+}
