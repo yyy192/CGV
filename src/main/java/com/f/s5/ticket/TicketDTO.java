a745297781d834eafd9e2ad7b64db7463afcc96d
AnJaeHyeonn <ahn9801@naver.com>
2021-09-30 17:26:28 +0900

    0000

--- src/main/java/com/f/s5/ticket/TicketDTO.java
+++ src/main/java/com/f/s5/ticket/TicketDTO.java
@@ -1,69 +1,80 @@
 package com.f.s5.ticket;
 
+import java.sql.Date;
+
 public class TicketDTO {
 
-   private String id;
-   private String ticketNum;
-   private String movieName;
-   private String theater;
-   private String timeTableSelete;
-   private String seat;
-   private String price;
+	private String id;
+	private String ticketNum;
+	private String movieName;
+	private String theater;
+	private Date watchDate;
+	private String timeTable;
+	private String seat;
+	private String price;
 
-   public String getId() {
-      return id;
-   }
+	public String getId() {
+		return id;
+	}
 
-   public void setId(String id) {
-      this.id = id;
-   }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-   public String getTicketNum() {
-      return ticketNum;
-   }
+	public String getTicketNum() {
+		return ticketNum;
+	}
 
-   public void setTicketNum(String ticketNum) {
-      this.ticketNum = ticketNum;
-   }
+	public void setTicketNum(String ticketNum) {
+		this.ticketNum = ticketNum;
+	}
 
-   public String getMovieName() {
-      return movieName;
-   }
+	public String getMovieName() {
+		return movieName;
+	}
 
-   public void setMovieName(String movieName) {
-      this.movieName = movieName;
-   }
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
 
-   public String getTheater() {
-      return theater;
-   }
+	public String getTheater() {
+		return theater;
+	}
 
-   public void setTheater(String theater) {
-      this.theater = theater;
-   }
+	public void setTheater(String theater) {
+		this.theater = theater;
+	}
 
-   public String getTimeTableSelete() {
-      return timeTableSelete;
-   }
+	public Date getWatchDate() {
+		return watchDate;
+	}
 
-   public void setTimeTableSelete(String timeTableSelete) {
-      this.timeTableSelete = timeTableSelete;
-   }
+	public void setWatchDate(Date watchDate) {
+		this.watchDate = watchDate;
+	}
 
-   public String getSeat() {
-      return seat;
-   }
+	public String getTimeTable() {
+		return timeTable;
+	}
 
-   public void setSeat(String seat) {
-      this.seat = seat;
-   }
+	public void setTimeTable(String timeTable) {
+		this.timeTable = timeTable;
+	}
 
-   public String getPrice() {
-      return price;
-   }
+	public String getSeat() {
+		return seat;
+	}
 
-   public void setPrice(String price) {
-      this.price = price;
-   }
+	public void setSeat(String seat) {
+		this.seat = seat;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
 
 }
